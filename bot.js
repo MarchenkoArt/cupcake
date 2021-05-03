8e5d45a9c8ab6808e5d3292c404453983e631e57
Artiom Marchenko <78684059+MarchenkoArt@users.noreply.github.com>
2021-05-03 23:43:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.author.username == 'MacGroundhog') {
     msg.react('🍌');
+    message.channel.send('@Жру по ночам#4711' + ' 🍌');
   }
 });
 
